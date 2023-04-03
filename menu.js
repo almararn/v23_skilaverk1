3512dfd8155d6d9141523c30aa5bc8be5b7690d5
almararn <almararn@gmail.com>
2023-04-03 19:12:10 +0000

    landing page at 40%

---
+++ menu.js
@@ -0,0 +1,20 @@
+
+let menu = document.getElementById('menu');
+let mobileMenu = document.getElementById('mobile-menu');
+let mobileMenuOpen = document.getElementById('hamburger');
+let mobileMenuClose = document.getElementById('x-hamburger');
+
+
+menu.addEventListener('click', function() {
+    if(mobileMenu.classList.contains('slide-in')) {
+        mobileMenu.classList.remove('slide-in');
+        mobileMenuClose.classList.remove('hidden');
+        mobileMenuOpen.classList.add('hidden');
+        console.log('slide-in')
+    }else{
+        mobileMenu.classList.add('slide-in');
+        mobileMenuClose.classList.add('hidden');
+        mobileMenuOpen.classList.remove('hidden');
+        console.log('slide-in-removed')
+    }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'llama-yellow': '#d4a040',
+        'llama-blue': '#429298',
+        'llama-brown': '#794f4a',
+        'llama-white': '#f4eedf',
+      },
+      backgroundImage: {
+        'logo-background': "url('/logo_back.png')",
+      },
+      fontFamily: {
+        "poppins": ["Poppins", "sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+}
+
